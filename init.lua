8da28d32e5f8d3c9e199b296f282a83f6aea9557
vecerniv <vecerniv@vscht.cz>
2025-07-21 14:49:40 +0200

    root switch

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,11 +102,14 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
-
+-- Tab settings
+vim.tabwidth = 4
+vim.shiftwidth = 4
+vim.tabstop = 4
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
